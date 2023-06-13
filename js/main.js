9b4f269ede3dc22b6baae8eedb57855882a2a6a2
Roland Estropia Jr <restropiajr@gmail.com>
2023-06-13 23:17:07 +0000

    Completed issue 1issue-1-user-can-view-game-information

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
+/* global valorantData */
 const $togglerButtons = document.querySelector('.toggler-buttons');
 const $togglerOpen = document.querySelector('.toggler-open');
 const $togglerClose = document.querySelector('.toggler-close');
-const $navbarLinks = document.querySelector('.navbar-links');
+const $navbarLinksMobile = document.querySelector('.navbar-links.mobile');
+const $landingPage = document.querySelector('[data-view="landing-page"]');
+const $valorantLogoNavbar = document.querySelector('#valorant-logo-navbar');
+const $scrollUpButton = document.querySelector('.scroll-up-button');
 
-// Event listener to show navbar links
+// navbarToggler function
 function navbarToggler() {
   if (event.target === $togglerOpen) {
-    $navbarLinks.classList.remove('hidden');
+    $navbarLinksMobile.classList.remove('hidden');
     $togglerOpen.classList.add('hidden');
     $togglerClose.classList.remove('hidden');
   } else if (event.target === $togglerClose) {
-    $navbarLinks.classList.add('hidden');
+    $navbarLinksMobile.classList.add('hidden');
     $togglerOpen.classList.remove('hidden');
     $togglerClose.classList.add('hidden');
   }
 }
+
+// Event listener to show navbar links
 $togglerButtons.addEventListener('click', navbarToggler);
+
+// scrollUpButtonToggler function
+function scrollUpButtonToggler() {
+  if (window.scrollY > 200) {
+    $scrollUpButton.classList.add('show');
+  } else {
+    $scrollUpButton.classList.remove('show');
+  }
+}
+
+// Event listener to show scroll up button
+window.addEventListener('scroll', scrollUpButtonToggler);
+
+// viewSwap function
+function viewSwap(view) {
+  if (view === 'landing-page') {
+    $landingPage.classList.remove('hidden');
+  }
+
+  valorantData.view = view;
+}
+
+// Event listener to swap to landing page
+$valorantLogoNavbar.addEventListener('click', viewSwap);
