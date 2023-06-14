5de7e24fda0aae4469ebd56d7c9489e3f4814008
Roland Estropia Jr <restropiajr@gmail.com>
2023-06-14 22:59:04 +0000

    Worked on DOM tree

--- js/main.js
+++ js/main.js
@@ -8,6 +8,15 @@ const $landingPage = document.querySelector('[data-view="landing-page"]');
 const $agentsPage = document.querySelector('[data-view="agents-page"]');
 const $valorantLogoNavbar = document.querySelector('.valorant-logo-navbar');
 const $scrollUpButton = document.querySelector('.scroll-up-button');
+const $abilities = document.querySelector('.abilities');
+const $abilityOne = document.querySelector('.ability-one');
+const $abilityTwo = document.querySelector('.ability-two');
+const $abilityThree = document.querySelector('.ability-three');
+const $abilityFour = document.querySelector('.ability-four');
+const $abilityOneDescription = document.querySelector('.ability-one-description');
+const $abilityTwoDescription = document.querySelector('.ability-two-description');
+const $abilityThreeDescription = document.querySelector('.ability-three-description');
+const $abilityFourDescription = document.querySelector('.ability-four-description');
 
 // navbarToggler function
 function navbarToggler(event) {
@@ -33,6 +42,15 @@ function scrollUpButtonToggler() {
     $scrollUpButton.classList.remove('show');
   }
 }
+
+// Event listener to hide navbar links (mobile view) if open and was not closed before going to desktop view.
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    $navbarLinksMobile.classList.add('hidden');
+    $togglerOpen.classList.remove('hidden');
+    $togglerClose.classList.add('hidden');
+  }
+});
 
 // Event listener to show scroll up button
 window.addEventListener('scroll', scrollUpButtonToggler);
@@ -65,5 +83,39 @@ $agentsLink.forEach(link => {
   });
 });
 
-// Function execution to view last viewed page
-viewSwap(valorantData.view);
+// Event listener to wait for HTML to parse before DOM manipulation
+document.addEventListener('DOMContentLoaded', () => {
+  viewSwap(valorantData.view);
+});
+
+// showAbilityDescription function
+function showAbilityDescription(event) {
+  if (event.target === $abilityOne) {
+    $abilityOneDescription.classList.remove('hidden');
+  } else if (event.target === $abilityTwo) {
+    $abilityTwoDescription.classList.remove('hidden');
+  } else if (event.target === $abilityThree) {
+    $abilityThreeDescription.classList.remove('hidden');
+  } else if (event.target === $abilityFour) {
+    $abilityFourDescription.classList.remove('hidden');
+  }
+}
+
+// Event listener to show ability description when hovered on ability
+$abilities.addEventListener('mouseover', showAbilityDescription);
+
+// unShowAbilityDescription function
+function unShowAbilityDescription(event) {
+  if (event.target === $abilityOne) {
+    $abilityOneDescription.classList.add('hidden');
+  } else if (event.target === $abilityTwo) {
+    $abilityTwoDescription.classList.add('hidden');
+  } else if (event.target === $abilityThree) {
+    $abilityThreeDescription.classList.add('hidden');
+  } else if (event.target === $abilityFour) {
+    $abilityFourDescription.classList.add('hidden');
+  }
+}
+
+// Event listener to show ability description when hovered on ability
+$abilities.addEventListener('mouseout', unShowAbilityDescription);
