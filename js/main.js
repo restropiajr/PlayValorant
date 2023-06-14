f23ecb6a9792d68b2715461379047c0d1d79e291
Roland Estropia Jr <restropiajr@gmail.com>
2023-06-14 07:25:58 +0000

    Added agents page view, executed viewSwap to show last viewed page, created title and subtitle for agents page

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ const $togglerButtons = document.querySelector('.toggler-buttons');
 const $togglerOpen = document.querySelector('.toggler-open');
 const $togglerClose = document.querySelector('.toggler-close');
 const $navbarLinksMobile = document.querySelector('.navbar-links.mobile');
+const $agentsLink = document.querySelectorAll('.agentsLink');
 const $landingPage = document.querySelector('[data-view="landing-page"]');
-const $valorantLogoNavbar = document.querySelector('#valorant-logo-navbar');
+const $agentsPage = document.querySelector('[data-view="agents-page"]');
+const $valorantLogoNavbar = document.querySelector('.valorant-logo-navbar');
 const $scrollUpButton = document.querySelector('.scroll-up-button');
 
 // navbarToggler function
-function navbarToggler() {
+function navbarToggler(event) {
   if (event.target === $togglerOpen) {
     $navbarLinksMobile.classList.remove('hidden');
     $togglerOpen.classList.add('hidden');
@@ -39,10 +41,29 @@ window.addEventListener('scroll', scrollUpButtonToggler);
 function viewSwap(view) {
   if (view === 'landing-page') {
     $landingPage.classList.remove('hidden');
+    $agentsPage.classList.add('hidden');
+  } else if (view === 'agents-page') {
+    $agentsPage.classList.remove('hidden');
+    $landingPage.classList.add('hidden');
   }
 
+  $navbarLinksMobile.classList.add('hidden');
+  $togglerOpen.classList.remove('hidden');
+  $togglerClose.classList.add('hidden');
   valorantData.view = view;
 }
 
 // Event listener to swap to landing page
-$valorantLogoNavbar.addEventListener('click', viewSwap);
+$valorantLogoNavbar.addEventListener('click', () => {
+  viewSwap('landing-page');
+});
+
+// Event listener to swap to agents page
+$agentsLink.forEach(link => {
+  link.addEventListener('click', () => {
+    viewSwap('agents-page');
+  });
+});
+
+// Function execution to view last viewed page
+viewSwap(valorantData.view);
